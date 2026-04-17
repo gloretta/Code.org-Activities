98b132b7876407765724daa66f763dabbc63b302
gloretta <osadchuk.gloria@gmail.com>
2026-04-17 14:57:44 -0700

    Create Team Class - Team.java

---
+++ Team Class - Team.java
@@ -0,0 +1,22 @@
+public class Team {
+
+  private String teamName;
+  private int numAthletes;
+
+
+  public Team() {
+    teamName = "new team";
+    numAthletes = 5;
+  }
+
+
+  public String getTeamName() {
+    return teamName;
+  }
+
+
+  public int getNumAthletes() {
+    return numAthletes;
+  }
+  
+}
