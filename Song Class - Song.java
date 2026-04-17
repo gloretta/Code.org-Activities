1dbd744a337386f80a1835bddb585e09c2c4e1ed
gloretta <osadchuk.gloria@gmail.com>
2026-04-17 14:47:33 -0700

    Create Song class with title and rating properties

---
+++ Song Class - Song.java
@@ -0,0 +1,19 @@
+public class Song {
+
+  private String title;
+  private int rating;
+
+  public Song() {
+    title = "new song";
+    rating = 1;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public int getRating() {
+    return rating;
+  }
+  
+}
