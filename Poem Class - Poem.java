b0200c6bcb7ae5615acfd3a90a36cd00c4f43eb8
gloretta <osadchuk.gloria@gmail.com>
2026-04-17 15:01:06 -0700

    Create Poem class with title and numLines properties

---
+++ Poem Class - Poem.java
@@ -0,0 +1,20 @@
+public class Poem {
+
+  private String title;
+  private int numLines;
+
+  public Poem() {
+    title = "new poem";
+    numLines = 1;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+
+  public int getNumLines() {
+    return numLines;
+  }
+  
+}
