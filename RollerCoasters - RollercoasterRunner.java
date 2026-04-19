35496a710cf3b48687fd7baf8bcb92ce1ab0b941
gloretta <osadchuk.gloria@gmail.com>
2026-04-18 18:48:30 -0700

    Add RollercoasterRunner class with main method

---
+++ RollerCoasters - RollercoasterRunner.java
@@ -0,0 +1,10 @@
+public class RollercoasterRunner {
+  public static void main(String[] args) {
+
+    Rollercoaster disney = new Rollercoaster("disney", 35);
+
+    System.out.println(disney.getName());
+    System.out.println(disney.getSpeed());
+    
+  }
+}
