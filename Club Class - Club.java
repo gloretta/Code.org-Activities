0d91bc5c30341c5e2573a3964e6e303f586e1142
gloretta <osadchuk.gloria@gmail.com>
2026-04-18 18:28:53 -0700

    Create Club Class - Club.java

---
+++ Club Class - Club.java
@@ -0,0 +1,20 @@
+public class Club {
+
+
+  private String sponsor;
+  private int numMembers;
+
+  public Club() {
+    sponsor = "new sponsor";
+    numMembers = 1;
+  }
+
+  public String getSponsor() {
+    return sponsor;
+  }
+
+  public int getNumMembers() {
+    return numMembers;
+  }
+  
+}
