1c279b66be853bf9c0c8806c9a868ab033a34c7c
gloretta <osadchuk.gloria@gmail.com>
2026-04-18 18:52:14 -0700

    Create National Parks - ParkRunner.java

---
+++ National Parks - ParkRunner.java
@@ -0,0 +1,9 @@
+public class ParkRunner {
+  public static void main(String[] args) {
+
+    NationalPark yellowStone = new NationalPark("Yellowstone", 2200000);
+    System.out.println(yellowStone.getName());
+    System.out.println(yellowStone.getArea());
+    
+  }
+}
