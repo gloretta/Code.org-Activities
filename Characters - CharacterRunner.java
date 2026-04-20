a4c4c2a8db10e119e2458c7963f2ea32cee77090
gloretta <osadchuk.gloria@gmail.com>
2026-04-19 20:53:39 -0700

    Create Characters - CharacterRunner.java

---
+++ Characters - CharacterRunner.java
@@ -0,0 +1,9 @@
+public class CharacterRunner {
+  public static void main(String[] args) {
+
+    Character kevin = new Character("Kevin", true);
+
+    System.out.println(kevin.getName());
+    System.out.println(kevin.hasLeadRole());
+  }
+}
