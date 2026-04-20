f4f66fe9a3c914ca5296e3d45b7fb72dd514d53e
gloretta <osadchuk.gloria@gmail.com>
2026-04-19 21:12:08 -0700

    Create Team - TeamRunner.java

---
+++ Team - TeamRunner.java
@@ -0,0 +1,15 @@
+public class TeamRunner {
+  public static void main(String[] args) {
+
+    /* ----------------------------------- TO DO -----------------------------------
+     * ✅ Instantiate a Team object using each parameterized constructor,
+     * then print the constructors and values assigned to the instance variables.
+     * -----------------------------------------------------------------------------
+     */
+    Team THS = new Team("THS Track and Field", 10);
+
+    System.out.println(THS.getName());
+    System.out.println(THS.getNumWins());
+    
+  }
+}
