6123ae340b3da6a3ee5c94a696d16260a91861b9
gloretta <osadchuk.gloria@gmail.com>
2026-04-29 20:58:51 -0700

    Create Student class with name and GPA attributes

---
+++ Student Creator - Student.java
@@ -0,0 +1,10 @@
+public class Student {
+
+  private String name;  // the name of the student
+  private double gpa;   // the student's gpa
+
+  public Student(String newName, double newGpa) {
+    name = newName;
+    gpa = newGpa;
+  }
+}
