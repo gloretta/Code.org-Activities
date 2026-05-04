42494b5e9714cd3ba2538b13dea9a28fd2c0fe1a
gloretta <osadchuk.gloria@gmail.com>
2026-05-03 21:29:30 -0700

    Add NeighborhoodRunner with PainterPlus initialization

---
+++ Placing Painter - NeighborhoodRunner.java
@@ -0,0 +1,9 @@
+import org.code.neighborhood.*;
+
+public class NeighborhoodRunner {
+  public static void main(String[] args) {
+
+    PainterPlus julia = new PainterPlus(2, 2, "south", 16);
+
+  }
+}
