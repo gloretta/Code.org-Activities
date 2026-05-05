e5da24eb8ff399c24f78f39ad8cca4162900aa4a
gloretta <osadchuk.gloria@gmail.com>
2026-05-04 21:07:36 -0700

    Add NeighborhoodRunner class with PainterPlus instance

---
+++ Create Painter (Donut) - NeighborhoodRunner.java
@@ -0,0 +1,9 @@
+import org.code.neighborhood.*;
+
+public class NeighborhoodRunner {
+  public static void main(String[] args) {
+
+    PainterPlus julia = new PainterPlus(0, 9, "north", 10);
+  
+  }
+}
