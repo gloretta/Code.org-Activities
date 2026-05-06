cf0571c632f20b55377c6e8d5d9d229a9d7b3e5f
gloretta <osadchuk.gloria@gmail.com>
2026-05-05 20:30:47 -0700

    Create Creating Painter (Traffic Cone) - NeighborhoodRunner.java

---
+++ Creating Painter (Traffic Cone) - NeighborhoodRunner.java
@@ -0,0 +1,9 @@
+import org.code.neighborhood.*;
+
+public class NeighborhoodRunner {
+  public static void main(String[] args) {
+
+    PainterPlus julia = new PainterPlus(5, 3, "west", 5);
+
+  }
+}
