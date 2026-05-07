cc3dec46a52cd9f3abaec50852c6b03bfb52f667
gloretta <osadchuk.gloria@gmail.com>
2026-05-06 18:18:50 -0700

    Add NeighborhoodRunner with PainterPlus instance

---
+++ Create Painter (In Front of the Red House) - NeighborhoodRunner.java
@@ -0,0 +1,10 @@
+import org.code.neighborhood.*;
+
+public class NeighborhoodRunner {
+  public static void main(String[] args) {
+
+    PainterPlus julia = new PainterPlus(3, 5, "east", 20);
+
+    
+  }
+}
