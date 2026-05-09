a3cc973d6a7eae0a43fd5f796e6a938e63c2817a
gloretta <osadchuk.gloria@gmail.com>
2026-05-08 18:54:17 -0700

    Create Dessert class with constructor and attributes
    Implement a Dessert class with flavor and price attributes.

---
+++ The Pie Class - Dessert.java
@@ -0,0 +1,20 @@
+public class Dessert {
+
+  private String flavor;    // The flavor of a dessert
+  private double price;     // The price of a dessert
+
+
+  public Dessert() {
+    this("plain", 0.50);
+  }
+
+  /*
+   * Sets flavor to the specified flavor and
+   * price to the specified price
+   */
+  public Dessert(String flavor, double price) {
+    this.flavor = flavor;
+    this.price = price;
+  }
+  
+}
