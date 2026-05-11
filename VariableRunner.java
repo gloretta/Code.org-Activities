4d437197eff014b4395c8cbbbdc5dbbafa4b8172
gloretta <osadchuk.gloria@gmail.com>
2026-05-10 21:40:16 -0700

    Create VariableRunner.java

---
+++ VariableRunner.java
@@ -0,0 +1,19 @@
+public class VariablesRunner {
+  public static void main(String[] args) {
+
+    int score = 125;
+    double temperature = 89.3;
+    boolean status = true;
+    String helloGreeting = "Hello World";
+    String welcomeGreeting = new String("Welcome to Java");
+    String name = "Gloria";
+
+    System.out.println("score: " + score);
+    System.out.println("temperature: " + temperature);
+    System.out.println("status: " + status);
+    System.out.println("helloGreeting: " + helloGreeting);
+    System.out.println("welcomeGreeting: " + welcomeGreeting);
+    System.out.println("name: " + name);
+
+  }
+}
