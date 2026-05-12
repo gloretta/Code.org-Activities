91a3ae49a827ac2087ca6b02dde182cc46622a2e
gloretta <osadchuk.gloria@gmail.com>
2026-05-11 19:14:46 -0700

    Create ModuloOperator.java

---
+++ ModuloOperator.java
@@ -0,0 +1,9 @@
+public class ExpressionsRunner {
+  public static void main(String[] args) {
+
+    int first = 4;
+    int second = 2;
+    int result = first % second;
+    System.out.println("first % second = " + result);
+  }
+}
