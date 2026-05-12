8b28568753592d85e7dea0c400d8e2c7352e70f3
gloretta <osadchuk.gloria@gmail.com>
2026-05-11 18:53:16 -0700

    Create ExtractingIndividualDigits.java

---
+++ ExtractingIndividualDigits.java
@@ -0,0 +1,39 @@
+public class ExpressionsRunner {
+  public static void main(String[] args) {
+
+    int divided = 5 * 2;
+    double doubleDivided = 5.0 * 2;
+
+    System.out.println("Integer 5 / 2 is " + divided);
+    System.out.println("Double 5 / 2 is " + doubleDivided);
+
+    System.out.println(3 - 5);
+    System.out.println(3 - 5.0);
+    System.out.println(3.0  - 5.0);
+    
+    System.out.println("High" + 5);
+    System.out.println("Version " + 2.0);
+    System.out.println("seventeen" + "teen");
+    
+    /* -------- */
+    System.out.println(5.6 + 2.4 * 5);
+    System.out.println("High" + 2 + 3);
+    System.out.println(1 + 1 + "nite");
+    System.out.println((5 + 2) * 5);
+    System.out.println(10 / (7 % 2 - 1.0));
+    System.out.println(-4.0 - -4 - 4);
+    System.out.println(8 / 2 * (2 + 2));
+    System.out.println(15 % 3 - 4);
+    System.out.println(5 * 2 - 6 + "ever");
+
+    int num = 12345;
+
+    System.out.println("Original number: " + num);
+    
+    while (num > 0) {
+        int lastDigit = num % 10;  
+        System.out.println("Extracted Digit: " + lastDigit);
+        num /= 10;  // Remove the last digit
+    }
+  }
+}
