bab0ebc98118a4674b6ad7943e4e436e56e5e4f9
gloretta <osadchuk.gloria@gmail.com>
2026-05-16 19:47:31 -0700

    Create DisplayRunner.java

---
+++ DisplayRunner.java
@@ -0,0 +1,8 @@
+public class DisplayRunner {
+    public static void main(String[] args) {
+
+      DisplayPrinter a = new DisplayPrinter(6);
+      System.out.println(a.printMessage("Gloria"));
+      
+    }
+}
