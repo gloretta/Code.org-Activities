33c899418df67417eb3d392e0b178614169671dc
gloretta <osadchuk.gloria@gmail.com>
2026-05-17 20:01:35 -0700

    Create HighScoreTrackerRunner.java

---
+++ HighScoreTrackerRunner.java
@@ -0,0 +1,8 @@
+public class HighScoreTrackerRunner{
+   public static void main(String[] args) {  
+
+     HighScoreTracker a = new HighScoreTracker(23);
+     System.out.println(a.increaseHighScore(230));
+
+   }
+}
