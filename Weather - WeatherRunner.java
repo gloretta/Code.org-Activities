90c690d5a825f00456fb5cb2ba519934737435ed
gloretta <osadchuk.gloria@gmail.com>
2026-05-25 21:53:42 -0700

    Create Weather - WeatherRunner.java

---
+++ Weather - WeatherRunner.java
@@ -0,0 +1,11 @@
+public class WeatherRunner {
+  public static void main(String[] args) {
+
+
+    // Creates a Weather object
+    Weather chicagoWeather = new Weather(57.1);
+    System.out.println(chicagoWeather);
+    
+    
+  }
+}
