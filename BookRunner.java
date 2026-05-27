45cf136d8f5d9308f5b40856395f7d9cb318b081
gloretta <osadchuk.gloria@gmail.com>
2026-05-26 21:18:51 -0700

    Add BookRunner class to demonstrate Book object

---
+++ BookRunner.java
@@ -0,0 +1,9 @@
+public class BookRunner {
+  public static void main(String[] args) {
+
+    // Creates a Book object
+    Book favBook = new Book("The Giver");
+    System.out.println(favBook);
+
+  }
+}
