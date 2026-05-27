8f3c32c1798f95e7fc69e5302bb1ac5f033da670
gloretta <osadchuk.gloria@gmail.com>
2026-05-26 21:21:20 -0700

    Create PlaylistRunner.java

---
+++ PlaylistRunner.java
@@ -0,0 +1,9 @@
+public class PlaylistRunner {
+  public static void main(String[] args) {
+
+    Playlist birthdayPlaylist = new Playlist(28);
+    
+    System.out.println(birthdayPlaylist);
+    
+  }
+}
