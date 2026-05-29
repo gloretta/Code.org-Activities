1d2474c0e1f5e60a22c0b952637aa5e83e8ac27d
gloretta <osadchuk.gloria@gmail.com>
2026-05-28 21:59:39 -0700

    Create 19 - The Cookie Class - FoodTruckRunner.java

---
+++ 19 - The Cookie Class - FoodTruckRunner.java
@@ -0,0 +1,12 @@
+public class FoodTruckRunner {
+  public static void main(String[] args) {
+
+    Dessert cheeseCake = new Dessert("chocolate", 10.99);
+    System.out.println(cheeseCake);
+
+
+    Cookie choco = new Cookie("chocolate", 2.99, true);
+    System.out.println(choco);
+    
+  }
+}
